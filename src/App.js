d0d2cf99b3d9c4722b676ff716756ee9da8d7500
Naceur <NaceurDjebali@gmail.com>
2019-01-26 20:52:04 +0100

    hw3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+}
+
 class App extends Component {
+
+  
+  
+
   render() {
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="App" style={{backgroundColor: background} }>
+
+      <p> Choose Computer Model </p>
+        
+        <select value={this.state}>
+          <option value=""> Choose Model </option> 
+          
+        </select>
+
+        <button onClick={this.state}> Add </button> 
+
       </div>
     );
   }
 }
+const background = '#696969'
 
-export default App;
+const mapStateToProps = state => {
+  return {
+  }
+};
+
+export default App
+
